f008e1b6c82b6fa9cb8d0899b4eef52a2f62fdea
aye4 <faction107@gmail.com>
2023-04-05 22:38:49 +0300

    Update main.py
    paint it black )

--- main.py
+++ main.py
@@ -5,40 +5,47 @@ from shutil import unpack_archive, ReadError
 from hashlib import md5
 
 FOLDERS = {
-           "images":   ('JPEG', 'PNG', 'JPG', 'SVG'),
-           "video":    ('AVI', 'MP4', 'MOV', 'MKV'),
-           "documents":('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'),
-           "audio":    ('MP3', 'OGG', 'WAV', 'AMR'),
-           "archives": ('ZIP', 'GZ', 'TAR')
-          }
+    "images": ("JPEG", "PNG", "JPG", "SVG"),
+    "video": ("AVI", "MP4", "MOV", "MKV"),
+    "documents": ("DOC", "DOCX", "TXT", "PDF", "XLSX", "PPTX"),
+    "audio": ("MP3", "OGG", "WAV", "AMR"),
+    "archives": ("ZIP", "GZ", "TAR"),
+}
 
 CYR = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
-TRN = ("a", "b", "v", "g", "d", "e", "e", "zh", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
-       "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+TRN = (
+    "a", "b", "v", "g", "d", "e", "e", "zh", "z", "i",
+    "j", "k", "l", "m", "n", "o", "p", "r", "s", "t",
+    "u", "f", "h", "ts", "ch", "sh", "sch", "", "y", "",
+    "e", "yu", "ya", "je", "i", "ji", "g",
+)
 trn_dict = {}
 for c, l in zip(CYR, TRN):
     trn_dict[ord(c)] = l
     trn_dict[ord(c.upper())] = l.upper()
 
-#list of archives to unpack
+# list of archives to unpack
 archives = []
-#counters
+# counters
 total = {}
 global_cntr_other = 0
 global_cntr_deleted = 0
 duplicate = [0, 0]
 
+
 def normalize(s: str) -> str:
-    return sub(r'\W', '_', s.translate(trn_dict))
+    return sub(r"\W", "_", s.translate(trn_dict))
+
 
 def plural(n: int):
     return n, " was" if n == 1 else "s were"
+
 
 def calc_hash(f: Path) -> str:
     BUF_SIZE = 128 * 1024
     md = md5()
     if f.is_file():
-        with open(f, 'rb') as x:
+        with open(f, "rb") as x:
             while True:
                 data = x.read(BUF_SIZE)
                 if not data:
@@ -46,37 +53,50 @@ def calc_hash(f: Path) -> str:
                 md.update(data)
     return md.hexdigest()
 
+
 def process_file(f: Path, target: Path):
     fname = normalize(f.stem)
     new_file = target / (fname + f.suffix)
-    #duplicate name check (extended for archives - to prevent from more than one archive being unpacked into the same folder)
-    if new_file.exists() or target.stem == 'archives' and list(target.glob(fname + '.*')):
-        #md5 hash
+    # duplicate name check (extended for archives - to prevent from more than one archive being unpacked into the same folder)
+    if (
+        new_file.exists()
+        or target.stem == "archives"
+        and list(target.glob(fname + ".*"))
+    ):
+        # md5 hash
         h = calc_hash(f)
-        #cycle to find a unique name (keep checking the hash)
+        # cycle to find a unique name (keep checking the hash)
         n = 0
         x = fname
-        while new_file.exists() or target.stem == 'archives' and (list(target.glob(x + '.*')) or (target / x).exists()):
-            if new_file.is_file() and new_file.stat().st_size == f.stat().st_size and calc_hash(new_file) == h:
-                #delete duplicate file (equal hash)
+        while (
+            new_file.exists()
+            or target.stem == "archives"
+            and (list(target.glob(x + ".*")) or (target / x).exists())
+        ):
+            if (
+                new_file.is_file()
+                and new_file.stat().st_size == f.stat().st_size
+                and calc_hash(new_file) == h
+            ):
+                # delete duplicate file (equal hash)
                 f.unlink()
                 duplicate[1] += 1
                 return
             else:
-                #filename pattern: "<old filename>_renamed_001_.<ext>"
+                # filename pattern: "<old filename>_renamed_001_.<ext>"
                 n += 1
-                x = fname + '_renamed_{:0>3}_'.format(n)
+                x = fname + "_renamed_{:0>3}_".format(n)
                 new_file = target / (x + f.suffix)
-        #renamed files counter
+        # renamed files counter
         duplicate[0] += 1
     else:
-        #moved files counters
+        # moved files counters
         total[target.stem] = total.get(target.stem, 0) + 1
-        #check/create sort folder
+        # check/create sort folder
         if not target.exists():
             target.mkdir()
-            print(f'Folder {target} has been created.')
-        #check if there is a file instead of a folder
+            print(f"Folder {target} has been created.")
+        # check if there is a file instead of a folder
         elif target.is_file():
             n = 1
             tmp = target.parent / (target.stem + str(n))
@@ -87,19 +107,20 @@ def process_file(f: Path, target: Path):
             target.mkdir()
             tmp.replace(target / target.stem)
             print(f'Folder "{target}" has been created.')
-            print(f'Warning: the file "{target}" was moved into the newly created folder.')
-    #move the file
+            print(f'Warning: the file "{target}" was moved into that folder.')
+    # move the file
     f.replace(new_file)
-    #add to unpack list
-    if target.stem == 'archives':
+    # add to unpack list
+    if target.stem == "archives":
         archives.append(new_file)
+
 
 def process_folder(f: Path, lvl: int) -> bool:
     global global_cntr_other, global_cntr_deleted
     d = bool(lvl)
     for x in f.iterdir():
         if x.is_dir():
-            if x.suffix or lvl or not x.stem.lower() in FOLDERS:
+            if lvl or x.suffix or not x.stem.lower() in FOLDERS:
                 d &= process_folder(x, lvl + 1)
         else:
             for s, e in FOLDERS.items():
@@ -107,60 +128,65 @@ def process_folder(f: Path, lvl: int) -> bool:
                     process_file(x, x.parents[lvl] / s)
                     break
             else:
-                #unlisted extention = do not move the file
+                # unlisted extention = do not move the file
                 global_cntr_other += 1
-                #mark folder as "not empty"
+                # mark folder as "not empty"
                 d = False
-                #normalize file name
+                # normalize file name
                 s = normalize(x.stem)
                 if s != x.stem:
                     x.rename(x.parent / (s + x.suffix))
     if d:
-        #delete empty folder
+        # delete empty folder
         f.rmdir()
         global_cntr_deleted += 1
     else:
-        #normalize folder name
+        # normalize folder name
         x = normalize(f.stem)
         if x != f.stem:
             f.rename(f.parent / (x + f.suffix))
     return d
-            
-if __name__ == '__main__':
-    #check parameter
+
+
+if __name__ == "__main__":
+    # check parameter
     if len(sys.argv) == 1:
-        print('Please specify the path to sort files from.')
-        exit(f'Usage: {sys.argv[0]} <path>')
+        print("Please specify the path to sort files from.")
+        exit(f"Usage: {sys.argv[0]} <path>")
     pth = Path(sys.argv[1])
-    #check the target folder
+    # check the target folder
     if not pth.exists():
-        exit(f'ERROR: The specified path ({sys.argv[1]}) does not exist.')
+        exit(f"ERROR: The specified path ({sys.argv[1]}) does not exist.")
     if not pth.is_dir():
-        exit(f'ERROR: The specified path ({sys.argv[1]}) is a file (not a folder).')
-    #main procedure
+        exit(f"ERROR: The specified path ({sys.argv[1]}) is a file (not a folder).")
+    # main procedure
     process_folder(pth, 0)
-    #process archives
+    # process archives
     for x in archives:
-       try:
-           unpack_archive(x, pth / "archives" / x.stem)
-       except ReadError:
-           print(f'Warning: could not unpack the file "{x}" (ReadError).')
-           archives.remove(x)
-       else:
-           x.unlink()
-    #print counters
+        try:
+            unpack_archive(x, pth / "archives" / x.stem)
+        except ReadError:
+            print(f'Warning: could not unpack the file "{x}" (ReadError).')
+            archives.remove(x)
+        else:
+            x.unlink()
+    # print counters
     for f, n in total.items():
-        if n and f != 'archives':
+        if n and f != "archives":
             print('{} file{} moved to the folder "{}".'.format(*plural(n), f))
     if len(archives):
-        print('{} archive{} unpacked.'.format(*plural(len(archives))))
+        print("{} archive{} unpacked.".format(*plural(len(archives))))
     if duplicate[0]:
-        print('{} file{} renamed due to duplicate name.'.format(*plural(duplicate[0])))
+        print("{} file{} renamed due to duplicate name.".format(*plural(duplicate[0])))
     if duplicate[1]:
-        print('{} duplicate file{} deleted.'.format(*plural(duplicate[1])))
+        print("{} duplicate file{} deleted.".format(*plural(duplicate[1])))
     if global_cntr_deleted:
-        print('{} empty folder{} deleted.'.format(*plural(global_cntr_deleted)))
+        print("{} empty folder{} deleted.".format(*plural(global_cntr_deleted)))
     if global_cntr_other:
-        print('{} file{} not moved due to unsupported type (extension).'.format(*plural(global_cntr_other)))
-    if not sum(x for x in total.values()) and not other and not sum(x for x in duplicate):
-        print('0 files found to process.')
+        print("{} file{} not moved due to unsupported extension.".format(*plural(global_cntr_other)))
+    if not (
+        global_cntr_other
+        or sum(x for x in duplicate)
+        or sum(x for x in total.values())
+    ):
+        print("0 files found to process.")
